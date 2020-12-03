599623a594e7cf26010f6238740ce857d636b939
David McCullough <david.mccullough@marzanoresearch.com>
2020-12-03 08:32:03 -0700

    12-3

--- Data processing script.R
+++ Data processing script.R
@@ -1,18 +1,53 @@
 library(readxl)
 library(dplyr)
+library(xlsx)
 
 state <- c("Kansas","Nebraska","Wyoming ","North Dakota")
 
 df <- data.frame()
 
+#Read in district poverty info
+dist_info <- read_excel("C:/Users/DavidMcCullough/Marzano Research/MZR - Share/10_REL/Task 6-Research/CE6.6_JiT/Data Entry/Analysis/school_districts_broadband_v2.xlsx")
+
+#Read in raw coding data
 for (states in state){
   
   df <- rbind(df,read_excel("C:/Users/DavidMcCullough/Marzano Research/MZR - Share/10_REL/Task 6-Research/CE6.6_JiT/Data Entry/Analysis/CE6.6.1 Data Entry_FINAL.xlsx", 
-                            sheet = states)[,1:26])
+                            sheet = states)[,1:29])
 }
 
+#Filter out uncoded plans
 df_coded <- df[!is.na(df$`Date Coded`),]
 
-dfna <- matrix(as.numeric(is.na(df_coded[,5:26])),nrow = 817,ncol=22)
-dists <- df_coded[rowSums(dfna) > 0,1:2] 
-write.csv(dists,file = "C:/Users/DavidMcCullough/Marzano Research/MZR - Share/10_REL/Task 6-Research/CE6.6_JiT/Data Entry/District_with_NAs.csv")
+#Match column names for dist info
+names(dist_info)[4] <- "District"
+names(dist_info)[46] <- "State"
+names(dist_info)[51] <- names(df_coded)[27]
+names(dist_info)[50] <- names(df_coded)[29]
+names(dist_info)[43] <- names(df_coded)[28]
+
+#Filter out irrelevant info columns
+relevent_dist_info <- dist_info[,c(4,43,46,50,51)]
+
+#Merge with coded dataset
+df_merged <- merge(df_coded[,1:26],relevent_dist_info)
+
+#Remove duplicate plans for reliability
+df_dupes_removed <- df_merged[-which((duplicated(df_merged[1]) | duplicated(df_merged[1],fromLast = TRUE)) & df_merged[4] != "Team"),]
+
+#Create dataframe for each state
+df_KS <- df_dupes_removed[df_dupes_removed[2] == "Kansas",]
+df_ND <- df_dupes_removed[df_dupes_removed[2] == "North Dakota",]
+df_WY <- df_dupes_removed[df_dupes_removed[2] == "Wyoming",]
+df_NE <- df_dupes_removed[df_dupes_removed[2] == "Nebraska",]
+
+#Create output files
+xlsx::write.xlsx(x = df_KS, file = "C:/Users/DavidMcCullough/Marzano Research/MZR - Share/10_REL/Task 6-Research/CE6.6_JiT/Data Entry/Analysis/output/Kansas_data.xlsx")
+xlsx::write.xlsx(x = df_ND, file = "C:/Users/DavidMcCullough/Marzano Research/MZR - Share/10_REL/Task 6-Research/CE6.6_JiT/Data Entry/Analysis/output/North_Dakota_data.xlsx")
+xlsx::write.xlsx(x = df_WY, file = "C:/Users/DavidMcCullough/Marzano Research/MZR - Share/10_REL/Task 6-Research/CE6.6_JiT/Data Entry/Analysis/output/Wyoming_data.xlsx")
+xlsx::write.xlsx(x = df_NE, file = "C:/Users/DavidMcCullough/Marzano Research/MZR - Share/10_REL/Task 6-Research/CE6.6_JiT/Data Entry/Analysis/output/Nebraska_data.xlsx")
+
+#Descriptive Stats
+Hmisc::describe(df_KS)
+
+
